95d1b3d72ff30e4fb8fc35fc88fe33ab246a5219
isadorasalles <isadorasalles@dcc.ufmg.br>
2020-08-23 20:17:22 -0300

    reestruturacao pagina historico e banco de dados

--- ouvICEx/init_flask.py
+++ ouvICEx/init_flask.py
@@ -2,52 +2,21 @@ from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 from scripts import analyses as anl
+from scripts.history import app_history
+from scripts.database import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.sqlite3'
+app.register_blueprint(app_history, url_prefix="")
 app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class posts(db.Model):
-    _id = db.Column("id", db.Integer, primary_key=True)
-    post = db.Column("post", db.String(500))
-    date = db.Column("date", db.Date)
-    author_dep = db.Column("author_dep", db.String(50))
-    ref_dep = db.Column("ref_dep", db.String(50))
-    context_t = db.Column("context_t", db.String(20))
-    situation_t = db.Column("situation_t", db.String(20))
-
-    def __init__(self, post, date, author_dep, ref_dep, context_t, situation_t):
-        self.post = post
-        self.date = date
-        self.author_dep = author_dep
-        self.ref_dep = ref_dep
-        self.context_t = context_t
-        self.situation_t = situation_t
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.sqlite3'
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-
-@app.route("/historico", methods=["POST", "GET"])
-def historico():
-    if request.method == "POST":
-        if request.form["dpt"]:
-            dpt = request.form["dpt"]
-            return render_template("historico.html", values=posts.query.filter_by(ref_dep=dpt))
-
-        elif request.form["limpar"]:
-            return render_template("historico.html", values=posts.query.all())
-    else:
-        return render_template("historico.html", values=posts.query.all())
-
-
 @app.route("/view")
 def view():
     return render_template("view.html", values = posts.query.all())
-
 
 @app.route("/form", methods=["POST", "GET"])
 def form():
@@ -66,8 +35,6 @@ def form():
     else:
         return render_template("form.html")
 
-<<<<<<< HEAD
-=======
 @app.route("/analyses", methods=["POST", "GET"])
 def analyses():
     #print("entrou")
@@ -81,8 +48,7 @@ def analyses():
     else:
         return "ERROR"
 
->>>>>>> 4d08a7f4eb968352e7bfedd8a35a9d6989e9536b
 if __name__ == "__main__":
-    db.create_all()
 
+    db.init_app(app)
     app.run(debug=True)
