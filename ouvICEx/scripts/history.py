7b105c87bad1b0fe317251592973650ca7d2f572
isadorasalles <isadorasalles@dcc.ufmg.br>
2020-08-24 22:12:32 -0300

    adicao do filtro tag de contexto

--- ouvICEx/scripts/history.py
+++ ouvICEx/scripts/history.py
@@ -6,22 +6,54 @@ app_history = Blueprint("app_history", __name__, template_folder="templates")
 @app_history.route("/history", methods=["POST", "GET"])
 def history():
     if request.method == "POST":
-        if request.form["ref_dpt"] == "0" and request.form["author_dpt"] == "0":
-            return render_template("historico.html", values=posts.query.all(),
-                    ref=db.session.query(posts.ref_dep.distinct()), author=db.session.query(posts.author_dep.distinct()))
-        elif request.form["author_dpt"] == "0":
-            ref_dpt = request.form["ref_dpt"]
-            return render_template("historico.html", values=posts.query.filter_by(ref_dep=ref_dpt),
-                    ref=db.session.query(posts.ref_dep.distinct()), author=db.session.query(posts.author_dep.distinct()))
-        elif request.form["ref_dpt"] == "0":
-            author_dpt = request.form["author_dpt"]
-            return render_template("historico.html", values=posts.query.filter_by(author_dep=author_dpt),
-                    ref=db.session.query(posts.ref_dep.distinct()), author=db.session.query(posts.author_dep.distinct()))
+        if request.form["context"] == "0":
+            if request.form["ref_dpt"] == "0" and request.form["author_dpt"] == "0":
+                return render_template("historico.html", values=posts.query.all(),
+                        ref=db.session.query(posts.ref_dep.distinct()), author=db.session.query(posts.author_dep.distinct()),
+                        context=db.session.query(posts.context_t.distinct()))
+
+            elif request.form["author_dpt"] == "0":
+                ref_dpt = request.form["ref_dpt"]
+                return render_template("historico.html", values=posts.query.filter_by(ref_dep=ref_dpt),
+                        ref=db.session.query(posts.ref_dep.distinct()), author=db.session.query(posts.author_dep.distinct()),
+                        context=db.session.query(posts.context_t.distinct()))
+
+            elif request.form["ref_dpt"] == "0":
+                author_dpt = request.form["author_dpt"]
+                return render_template("historico.html", values=posts.query.filter_by(author_dep=author_dpt),
+                        ref=db.session.query(posts.ref_dep.distinct()), author=db.session.query(posts.author_dep.distinct()),
+                        context=db.session.query(posts.context_t.distinct()))
+            else:
+                ref_dpt = request.form["ref_dpt"]
+                author_dpt = request.form["author_dpt"]
+                return render_template("historico.html", values=posts.query.filter_by(author_dep=author_dpt, ref_dep=ref_dpt),
+                        ref=db.session.query(posts.ref_dep.distinct()), author=db.session.query(posts.author_dep.distinct()),
+                        context=db.session.query(posts.context_t.distinct()))
         else:
-            ref_dpt = request.form["ref_dpt"]
-            author_dpt = request.form["author_dpt"]
-            return render_template("historico.html", values=posts.query.filter_by(author_dep=author_dpt, ref_dep=ref_dpt),
-                    ref=db.session.query(posts.ref_dep.distinct()), author=db.session.query(posts.author_dep.distinct()))
+            cont = request.form["context"]
+            if request.form["ref_dpt"] == "0" and request.form["author_dpt"] == "0":
+                return render_template("historico.html", values=posts.query.filter_by(context_t=cont),
+                        ref=db.session.query(posts.ref_dep.distinct()), author=db.session.query(posts.author_dep.distinct()),
+                        context=db.session.query(posts.context_t.distinct()))
+
+            elif request.form["author_dpt"] == "0":
+                ref_dpt = request.form["ref_dpt"]
+                return render_template("historico.html", values=posts.query.filter_by(ref_dep=ref_dpt, context_t=cont),
+                        ref=db.session.query(posts.ref_dep.distinct()), author=db.session.query(posts.author_dep.distinct()),
+                        context=db.session.query(posts.context_t.distinct()))
+
+            elif request.form["ref_dpt"] == "0":
+                author_dpt = request.form["author_dpt"]
+                return render_template("historico.html", values=posts.query.filter_by(author_dep=author_dpt, context_t=cont),
+                        ref=db.session.query(posts.ref_dep.distinct()), author=db.session.query(posts.author_dep.distinct()),
+                        context=db.session.query(posts.context_t.distinct()))
+            else:
+                ref_dpt = request.form["ref_dpt"]
+                author_dpt = request.form["author_dpt"]
+                return render_template("historico.html", values=posts.query.filter_by(author_dep=author_dpt, ref_dep=ref_dpt, context_t=cont),
+                        ref=db.session.query(posts.ref_dep.distinct()), author=db.session.query(posts.author_dep.distinct()),
+                        context=db.session.query(posts.context_t.distinct()))
     else:
         return render_template("historico.html", values=posts.query.all(),
-                ref=db.session.query(posts.ref_dep.distinct()), author=db.session.query(posts.author_dep.distinct()))
+                ref=db.session.query(posts.ref_dep.distinct()), author=db.session.query(posts.author_dep.distinct()),
+                context=db.session.query(posts.context_t.distinct()))
