61e177f7e3128ac1ed15e1144499253e6d6fec8c
Igor Marques <igorma@ufmg.br>
2020-08-26 22:02:42 -0300

    brainstorms de primeiros botões funcionais de exibição de gráficos

--- ouvICEx/scripts/analyses.py
+++ ouvICEx/scripts/analyses.py
@@ -5,15 +5,23 @@ from scripts.database import posts, db
 app_analyses = Blueprint("app_analyses", __name__, template_folder="templates")
 @app_analyses.route("/analyses", methods=["POST", "GET"])
 def analyses():
+	teste = values = posts.query.all()
 	if request.method == "GET":
 		return render_template(
 			"analyses.html",
 	 		values = posts.query.all()
 		)
 	elif request.method == "POST":
-		btn = request.form["btn_plot"]
-		return str(btn)
-		#return render_template(
-		#	"form.html",
-	 	#	values = posts.query.all()
-		#)
+		btn = str(request.form["btn_plot"])
+		
+		if btn == "Exibir scatter":
+			grafico = "nesse caso viria um scatter"
+		elif  btn == "Exibir barplot":
+			grafico = "nesse caso viria um barplot"
+			
+		return render_template(
+			"analyses.html",
+	 		values = posts.query.all(),
+	 		grafico = grafico
+		)
+		
