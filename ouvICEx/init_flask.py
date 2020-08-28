d77e91c3744d49758e7b54f5201e3b451dc764a2
anaflavia.silva <anaflavia.dmiranda@gmail.com>
2020-08-28 19:15:19 -0300

    refatoracao form

--- ouvICEx/init_flask.py
+++ ouvICEx/init_flask.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
-import datetime
 from scripts import analyses as anl
 from scripts.history import app_history
+from scripts.form import app_form
 from scripts.database import db, posts
 
 app = Flask(__name__)
 app.register_blueprint(app_history, url_prefix="")
+app.register_blueprint(app_form, url_prefix="")
 app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.sqlite3'
 
@@ -17,23 +18,6 @@ def home():
 @app.route("/view")
 def view():
     return render_template("view.html", values = posts.query.all())
-
-@app.route("/form", methods=["POST", "GET"])
-def form():
-    if request.method == "POST":
-        post = request.form["post"]
-        date = datetime.date.today()
-        author_dep = request.form["author_dep"]
-        ref_dep = request.form["ref_dep"]
-        context_t = request.form["context_t"]
-        situation_t = request.form["situation_t"]
-        envio = posts(post, date, author_dep, ref_dep, context_t, situation_t)
-        db.session.add(envio)
-        db.session.commit()
-        #flash("Informações enviadas!")
-        return render_template("form.html")
-    else:
-        return render_template("form.html")
 
 @app.route("/analyses", methods=["POST", "GET"])
 def analyses():
@@ -49,6 +33,7 @@ def analyses():
         return "ERROR"
 
 if __name__ == "__main__":
-
     db.init_app(app)
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
