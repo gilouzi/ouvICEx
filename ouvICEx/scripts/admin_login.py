f6d4c31f9fae6d1a35aa29d9c37195a4dad9c2fe
Thiago Martin Poppe <thiagopoppe@dcc.ufmg.br>
2020-08-28 16:34:05 -0300

    Adicionando form para login

--- ouvICEx/scripts/admin_login.py
+++ ouvICEx/scripts/admin_login.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template, flash
 
 admin_login = Blueprint("admin_login", __name__, template_folder='templates/')
 
-@admin_login.route('/login')
+@admin_login.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('admin_login.html')
