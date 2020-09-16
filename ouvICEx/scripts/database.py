d7d77e58f60331647f406a5c1e323f55a6d0b62d
anaflavia.silva <anaflavia.dmiranda@gmail.com>
2020-09-16 10:27:43 -0300

    adicionando a possibilidade de os envios serem anonimos ou nao

--- ouvICEx/scripts/database.py
+++ ouvICEx/scripts/database.py
@@ -5,14 +5,16 @@ db = SQLAlchemy()
 class posts(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
     post = db.Column("post", db.String(500))
+    name = db.Column("name", db.String(100))
     date = db.Column("date", db.Date)
     author_dep = db.Column("author_dep", db.String(50))
     ref_dep = db.Column("ref_dep", db.String(50))
     context_t = db.Column("context_t", db.String(20))
     situation_t = db.Column("situation_t", db.Boolean)
 
-    def __init__(self, post, date, author_dep, ref_dep, context_t, situation_t):
+    def __init__(self, post, name, date, author_dep, ref_dep, context_t, situation_t):
         self.post = post
+        self.name = name
         self.date = date
         self.author_dep = author_dep
         self.ref_dep = ref_dep
