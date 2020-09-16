a04292383abae6fa757a0b8529ff4a62397a63f2
Thiago Martin Poppe <thiagopoppe@dcc.ufmg.br>
2020-09-16 20:54:39 -0300

    Adicionando flash messages

--- ouvICEx/scripts/admin.py
+++ ouvICEx/scripts/admin.py
@@ -8,25 +8,32 @@ def login():
     if request.method == 'POST':
         user = request.form['user']
         pwd = request.form['pwd']
+
         found_user = users.query.filter_by(user=user, password=pwd).first()
         if found_user:
             session['user'] = user
             session['pwd'] = pwd
+            flash("Bem vindo novamente, %s!" % (session['user']))
             return redirect(url_for('admin.view'))
         else:
             flash("Usuário e/ou senha incorreto, tente novamente")
             return render_template('admin_login.html')
     else:
         if 'user' in session:
+            flash("Você já se encontra logado no sistema!")
             return redirect(url_for('admin.view'))
 
     return render_template('admin_login.html')
 
 @admin.route('/logout')
 def logout():
+    if 'user' in session:
+        flash("Logout feito com sucesso!")
+
     session.pop('user', None)
     session.pop('pwd', None)
     return redirect(url_for("home"))
+    return redirect(url_for("admin.login"))
 
 @admin.route('/')
 @admin.route('/view')
@@ -34,4 +41,7 @@ def view():
     if 'user' in session:
         return render_template('admin_view.html')
     
+        values = posts.query.all()
+        return render_template('admin_view.html', posts=values)
+        
     return redirect(url_for("admin.login"))
