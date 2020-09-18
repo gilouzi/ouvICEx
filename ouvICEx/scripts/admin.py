3ae2c69552eea46e936144824c15b9edc3df756d
Giovanna Bellonia <giovannabellonia@gmail.com>
2020-09-18 16:35:43 -0300

    mudando o blueprint do admin

--- ouvICEx/scripts/admin.py
+++ ouvICEx/scripts/admin.py
@@ -2,9 +2,9 @@ from flask import Blueprint, render_template, redirect, url_for, request, sessio
 from scripts.history import return_request
 from scripts.database import db, posts, users
 
-admin = Blueprint("admin", __name__, template_folder='templates/')
+app_admin = Blueprint("admin", __name__, template_folder='templates/')
 
-@admin.route('/login', methods=['POST', 'GET'])
+@app_admin.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         user = request.form['user']
@@ -26,7 +26,7 @@ def login():
 
     return render_template('admin_login.html')
 
-@admin.route('/logout')
+@app_admin.route('/logout')
 def logout():
     if 'user' in session:
         flash("Logout feito com sucesso!", "success")
@@ -35,7 +35,7 @@ def logout():
     session.pop('pwd', None)
     return redirect(url_for("admin.login"))
 
-@admin.route('/changeStatus/<int:pid>')
+@app_admin.route('/changeStatus/<int:pid>')
 def changeStatus(pid):
     if 'user' in session:
         post = posts.query.get(pid)
@@ -47,8 +47,8 @@ def changeStatus(pid):
         
     return redirect(url_for("admin.view"))
 
-@admin.route('/', methods=["POST", "GET"])
-@admin.route('/view', methods=["POST", "GET"])
+@app_admin.route('/', methods=["POST", "GET"])
+@app_admin.route('/view', methods=["POST", "GET"])
 def view():
     if 'user' in session:
         if request.method == "POST":
