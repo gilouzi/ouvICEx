106a66a47bcf8b3df69b8bf77966bc76c637f57b
isadorasalles <isadorasalles@dcc.ufmg.br>
2020-09-18 14:12:43 -0300

    refatoração de código historico

--- ouvICEx/scripts/history.py
+++ ouvICEx/scripts/history.py
@@ -5,7 +5,7 @@ import datetime
 
 app_history = Blueprint("app_history", __name__, template_folder="templates")
 
-def return_values(ref_dpt, author_dpt, context, situation, date_start, date_end):
+def db_filters(ref_dpt, author_dpt, context, situation, date_start, date_end):
 
     if (date_start != None and date_end != None):
         query = db.session.query(posts).filter(posts.date>=date_start, posts.date<=date_end)
@@ -27,30 +27,35 @@ def return_values(ref_dpt, author_dpt, context, situation, date_start, date_end)
 
     return query
 
+def return_request(request):
+    ref_dpt = None
+    author_dpt = None
+    context = None
+    situation = None
+    date_start = None
+    date_end = None
+    if request.form["ref_dpt"] != "0":
+        ref_dpt = request.form["ref_dpt"]
+    if request.form["author_dpt"] != "0":
+        author_dpt = request.form["author_dpt"]
+    if request.form["context"] != "0":
+        context = request.form["context"]
+    if request.form["situation"] != "0":
+        situation = request.form["situation"]
+    if request.form["start"]:
+        date_start = request.form["start"]
+    if request.form["end"]:
+        date_end = request.form["end"]
+
+    return db_filters(ref_dpt, author_dpt, context, situation, date_start, date_end)
+
 @app_history.route("/history", methods=["POST", "GET"])
 def history():
     dict_ref = {}
-    if request.method == "POST":
-        ref_dpt = None
-        author_dpt = None
-        context = None
-        situation = None
-        date_start = None
-        date_end = None
-        if request.form["ref_dpt"] != "0":
-            ref_dpt = request.form["ref_dpt"]
-        if request.form["author_dpt"] != "0":
-            author_dpt = request.form["author_dpt"]
-        if request.form["context"] != "0":
-            context = request.form["context"]
-        if request.form["situation"] != "0":
-            situation = request.form["situation"]
-        if request.form["start"]:
-            date_start = request.form["start"]
-        if request.form["end"]:
-            date_end = request.form["end"]
 
-        values_db = return_values(ref_dpt, author_dpt, context, situation, date_start, date_end)
+    if request.method == "POST":
+        values_db = return_request(request)
+
         return render_template("history.html", values=values_db,
                 ref=db.session.query(posts.ref_dep.distinct()),
                 author=db.session.query(posts.author_dep.distinct()),
